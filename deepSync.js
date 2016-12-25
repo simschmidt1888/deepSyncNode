1a5400fafa5afd5f7f71af7e2c6e4286b816d5ea
smithsf0x <smithsf0x@gmail.com>
2016-12-25 11:23:17 -0500

    start...

--- deepSync.js
+++ deepSync.js
@@ -1,4 +1,18 @@
-function deepSync(){
+function _deepSync(){
   this.test = "asdf";
-
+  
+  this.logType = {
+    INFO: 0,
+    WARN: 1,
+    ERR:  2
+  };
+  this.log = function(type, text){
+    var pre = "" + new Date();
+    
+    console.log( pre + "  " + text + "");
+  }
 }
+
+
+var deepSync = new _deepSync();
+deepSync.log(0, "test");
